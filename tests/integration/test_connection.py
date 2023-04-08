1944ec87a32d2fd524b476ecb8b21dab0db061da
Asher Norland <asher.norland@gmail.com>
2023-04-08 14:09:13 -0400

    Update Test

--- tests/integration/test_connection.py
+++ tests/integration/test_connection.py
@@ -3,13 +3,18 @@
 
 from typing import Any
 
+import pytest
+
 
 class TestConnection:
     """Verifies connections"""
 
-    def test_connections(self, host: Any) -> None:
+    def test_require_ansible(self, host: Any) -> None:
         """Tests host connection
 
         Args:
             host: The Testinfra host
         """
+
+        with pytest.raises(RuntimeError):
+            host.ansible("setup")
